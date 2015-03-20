2463d4f7331cea25fc38f4eeea602ff4c5662f81
Nick Manos <manos.nick@gmail.com>
2015-03-19 17:56:53 -0700

    photos fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function() {
 	$("#faqAccordion").accordion({ active: 0, heightStyle: "content" });
-	  
+
 	$(function(){
 		$("#slidesLeft").slidesjs({
 			height:250,
@@ -16,7 +16,7 @@
 				}
 			});
 		});
-		
+
 	$(function(){
 		$("#slidesRight").slidesjs({
 			height:250,
@@ -32,20 +32,20 @@
 				}
 			});
 		});
-		
+
 	$("window").load(function() {
 		var elem = document.getElementById("topRight");
 		elem.setAttribute("float: right;")
 		});
-		
+
 	/** Takes a variable number of arguments (the css id of desired elements) then cycles through them calling 'show' to make the content appear. */
 	function display() {
 		for (var i = 0, j = arguments.length; i < j; i++){
 		var el = $(arguments[i]);
         el.show('slide');
 		}
-	};	
-	
+	};
+
 	/** Takes a variable number of arguments (the css id of desired elements) then cycles through them calling 'hide' to make the content disappear.  */
 	function hide() {
 		for (var i = 0, j = arguments.length; i < j; i++){
@@ -53,72 +53,63 @@
         el.hide('slide')
 		}
 	};
-	
+
 	/** Determines if the 3 pictures that are removed for the 'New Homes' section are visible or not.*/
 	function isHidden(){
 		if ($('#remove1').css('display') === 'none') {
 			return true;
 		} else {return false;}
 	};
-	
+
 	/** Switches the thumbnail pictures as the user selects different catagories. */
 	function switchThumbnails(numberOfPictures, path){
-		var pic = '#pic';
-		var commonPath = 'media/portfolio/';
-	
+
 		for (i=0; i < numberOfPictures; i++ ){
-			
-			fullPath = commonPath;
-			numReplace = i+1;
-			pathReplace = path + numReplace + '.jpg';
-			
-			picNum = pic + numReplace;
-			fullPath = fullPath + pathReplace;
-			
-			$(picNum).attr('src', fullPath);
+			fullPath = 'media/portfolio/' + path + (i+1) + '.jpg';
+			$('#pic' + (i+1)).attr('src', fullPath);
 		}
 	}
-	
+
 	/** Changes the pictures for the gallery's main display. */
 	$('.thumbnail').click(function(event){
 		event.preventDefault();
 		var src = $(this).attr('src');
 		var oldSrc = $('#slideshow').attr('src');
-		
+
 		$('#slideshow').slideUp(function(){
 			$('#slideshow').attr('src', src).slideDown();
 		});
-	});	
-		
-		
+	});
+
+
 	$('#bathPics').click(function(){
 		display('#remove1', '#remove2', '#remove3', '#remove4', '#remove5', '#remove6');
 		switchThumbnails(10, 'baths/Bath');
 	});
-	
+
 	$('#exteriorPics').click(function(){
 		hide('#remove4', '#remove5', '#remove6');
 		if (isHidden()) {display('#remove1', '#remove2', '#remove3')};
 		switchThumbnails(7, 'exteriors-additions/Exteriors-Additions');
 	});
-	
+
 	$('#interiorPics').click(function(){
 		hide('#remove4', '#remove5', '#remove6');
 		if (isHidden()) {display('#remove1', '#remove2', '#remove3')};
 		switchThumbnails(7, 'interiors/Interior')
 	});
-	
+
 	$('#kitchenPics').click(function(){
 		hide('#remove4', '#remove5', '#remove6');
 		if (isHidden()) {display('#remove1', '#remove2', '#remove3')};
 		switchThumbnails(7, 'kitchens/Kitchen')
 	});
-	
-	$('#newHomePics').click(function(){		
+
+	$('#newHomePics').click(function(){
 		hide('#remove1', '#remove2', '#remove3', '#remove4', '#remove5', '#remove6');
 		switchThumbnails(4, 'new-homes/NewHomes')
 	});
-	
+
 	$('#SpecialUsePics').click(function(){
 		hide('#remove4', '#remove5', '#remove6');
 		if (isHidden()) {display('#remove1', '#remove2', '#remove3')};
@@ -163,6 +154,29 @@
 		$('#SpecialUsePics').toggleClass('animated pulse infinite hoverPhotoHeader')
 	})
 
+	$('#pic7').click(function(){
+		elem = $('#pic7').attr('src')
+		if (elem = 'media/portfolio/baths/Bath7.jpg'){
+			slideshow = document.getElementById('#slideshow');
+			slideshow.style.width = '500px';
+			slideshow.style.marginLeft = '100px';
+		}
+	})
+
+	$('#pic10').click(function(){
+		elem = $('#pic10').attr('src')
+		if (elem = 'media/portfolio/baths/Bath7.jpg'){
+			slideshow = document.getElementById('#slideshow');
+			slideshow.style.width = '500px';
+			slideshow.style.marginLeft = '100px';
+		}
+	})
+
+	function resetSlideShowStyle(){
+			slideshow = document.getElementById('#slideshow');
+			slideshow.style.width = '750px';
+			slideshow.style.marginLeft = '';
+	}
 
 });
 
